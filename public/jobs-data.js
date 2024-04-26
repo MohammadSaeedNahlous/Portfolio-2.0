a030674b53673fced6a8e2c37edd8a2e2a4f2674
MoNahlous <jay.haji041@gmail.com>
2024-04-26 17:02:23 +0200

    update info

--- public/jobs-data.js
+++ public/jobs-data.js
@@ -1,25 +1,26 @@
 export const JOB_DATA = [
+
   {
     id: 1,
-    title: "Master's Degree Student",
-    companyName: 'SVU',
+    title: 'Frontend Developer',
+    companyName: 'Robotic Minds',
     startDate: 'February 2021',
-    endDate: 'Present',
+    endDate: 'November 2022',
     Responsibilities: [
-      'Building Full-Stack Projects for my classes',
-      'Implementing a variety of technologies such as Data mining algorithms,Information retrieval algorithms, Parallel systems, android project and more using MERN Stack and other platforms and languages',
+      'Write modern, high-performance, and maintainable code for both new and existing projects. This includes upgrading projects from ASP .NET to ReactJS and accurately transforming UX/UI designs into web pages and components.',
+      'Building Optimized Progressive Web Apps.',
+      'Building Projects with ReactJS and Maintaining Them.',
     ],
   },
   {
     id: 2,
-    title: 'Frontend Developer',
-    companyName: 'Robotic Minds',
-    startDate: 'February 2021',
+    title: "Frontend Developer",
+    companyName: 'Blue Tech',
+    startDate: 'November 2022',
     endDate: 'Present',
     Responsibilities: [
-      'Write modern, performant, maintainable code for old and new projects',
-      'Building UI for IOT projects',
-      'Building Projects With ReactJs and maintaining it',
+      'Leveraging ReactJS, NextJS, and PWA, along with technologies like Redux and Map API, to construct and optimize web applications with enhanced performance and user experience.',
+      'Collaborating effectively with a diverse team that employs various software development methodologies and tools, including Agile and Jira, to achieve optimal project outcomes.',
     ],
   },
 ];
--- public/projects-data.js
+++ public/projects-data.js
@@ -1,80 +1,121 @@
 export const PROJECT_DATA = [
     {
-        id:'p1',
+        id: 'p1',
         title: 'GitHub Finder',
         description: 'ReactJs project that consumes GitHub API, You can search for people Accounts on GitHub and retrieve their information, repositories, if they are hireable or not, and more!',
-        tags: ['ReactJs','Context-API', 'GitHub API'],
+        tags: ['ReactJs', 'Context-API', 'GitHub API'],
         url: 'https://github.com/MoSaeedNahlous/GitHub-Finder',
-        projectUrl:'https://react-app-github-finder-app.netlify.app/'
+        projectUrl: 'https://react-app-github-finder-app.netlify.app/'
     },
     {
-        id:'p2',
+        id: 'p2',
         title: 'Form Validation',
         description: 'Basic HTML Form with validation and custom alerts, It was for learning purposes ',
         tags: ['HTML5', 'CSS3'],
-        url:'https://github.com/MoSaeedNahlous/Form-Validator'
+        url: 'https://github.com/MoSaeedNahlous/Form-Validator'
     },
     {
-        id:'p3',
+        id: 'p3',
         title: 'Balance Tracker',
         description: "This is a basic react app that helps you to track your balance/expenses, it's not fully completed, more updates soon!",
         tags: ['ReactJs'],
-        url:'https://github.com/MoSaeedNahlous/balance-tracker'
+        url: 'https://github.com/MoSaeedNahlous/balance-tracker'
     },
     {
-        id:'p4',
+        id: 'p4',
         title: 'Acme website',
         description: 'My first website used basic HTML5 and CSS3',
         tags: ['HTML5', 'CSS3'],
-        url:'https://github.com/MoSaeedNahlous/acme'
+        url: 'https://github.com/MoSaeedNahlous/acme'
     },
     {
-        id:'p5',
+        id: 'p5',
         title: 'Travel Map App',
         description: "MERN fullstack web app that consume MapBox API, You can discover new places, Check other user's reviews, and add your reviews too!",
-        tags: ['ReactJs', 'NodeJs' ,'ExpressJs', 'MongoDB' , 'MapBox API'],
-        url:'https://github.com/MoSaeedNahlous/MERN-Travel-Map-App'
+        tags: ['ReactJs', 'NodeJs', 'ExpressJs', 'MongoDB', 'MapBox API'],
+        url: 'https://github.com/MoSaeedNahlous/MERN-Travel-Map-App'
     },
     {
-        id:'p6',
+        id: 'p6',
         title: 'E-Commerce',
         description: "MERN full-stack E-commerce web app with orders, products, payment with credits, I used Redux, React-Bootstrap in addition to MERN technologies",
-        tags: ['ReactJs', 'React-Bootstrap','NodeJs' ,'ExpressJs', 'MongoDB' ,'Redux','JWT'],
-        url:'https://github.com/MoSaeedNahlous/MERN-E-commerce'
+        tags: ['ReactJs', 'React-Bootstrap', 'NodeJs', 'ExpressJs', 'MongoDB', 'Redux', 'JWT'],
+        url: 'https://github.com/MoSaeedNahlous/MERN-E-commerce'
     },
     {
-        id:'p7',
+        id: 'p7',
         title: 'My Portfolio 1.0',
         description: "My first and oldest portfolio, its collecting dust because its empty XD",
         tags: ['HTML5', 'CSS3'],
-        url:'https://github.com/MoSaeedNahlous/Portfolio'
+        url: 'https://github.com/MoSaeedNahlous/Portfolio'
     },
     {
-        id:'p8',
+        id: 'p8',
         title: 'Advanced information retrieval class project',
         description: "MERN full-stack web app project for my AIR class, I used three IR algorithms: Boolean, Extended Boolean and VSM",
-        tags: ['ReactJs','NodeJs' ,'ExpressJs', 'MongoDB'],
-        url:'https://github.com/MoSaeedNahlous/AIR-Project'
+        tags: ['ReactJs', 'NodeJs', 'ExpressJs', 'MongoDB'],
+        url: 'https://github.com/MoSaeedNahlous/AIR-Project'
     },
     {
-        id:'p9',
+        id: 'p9',
         title: 'Advanced data exchange class siminar',
         description: "My part of my Advanced Data Exchange seminar class, my first demo with GraphQL and Apollo",
-        tags: ['ReactJs', 'GraphQL','Apollo'],
-        url:'https://github.com/MoSaeedNahlous/ADE-project'
+        tags: ['ReactJs', 'GraphQL', 'Apollo'],
+        url: 'https://github.com/MoSaeedNahlous/ADE-project'
     },
     {
-        id:'p10',
+        id: 'p10',
         title: 'Advanced Mobile Web class project',
         description: "My part of my Advanced Mobile Web class project is MERN full-stack web app about traffic tickets management",
-        tags: ['ReactJs','NodeJs' ,'ExpressJs', 'MongoDB' ,'Redux','JWT'],
-        url:'https://github.com/MoSaeedNahlous/AMW-Project'
+        tags: ['ReactJs', 'NodeJs', 'ExpressJs', 'MongoDB', 'Redux', 'JWT'],
+        url: 'https://github.com/MoSaeedNahlous/AMW-Project'
     },
     {
-        id:'p11',
+        id: 'p11',
         title: 'Social Media App',
         description: "MERN full-stack Social Media web app with Chat Messenger! ",
-        tags: ['ReactJs','NodeJs' ,'ExpressJs', 'MongoDB','Context-API','Socket.IO'],
-        url:'https://github.com/MoSaeedNahlous/MERN---Social-Media-App'
+        tags: ['ReactJs', 'NodeJs', 'ExpressJs', 'MongoDB', 'Context-API', 'Socket.IO'],
+        url: 'https://github.com/MoSaeedNahlous/MERN---Social-Media-App'
+    }, {
+        id: 'p12',
+        title: 'AZ Dashboard',
+        description: "A full complex Dashboard with alot of functions to manage an online clothing store. ",
+        tags: ['ReactJs', 'Redux'],
+
+    }, {
+        id: 'p13',
+        title: 'Marten Do Delivery Boy App',
+        description: "PWA app for delivery employees",
+        tags: ['HTML5', 'CSS3', 'JavaScript', 'PWA'],
+
+
+    }, {
+
+        id: 'p14',
+        title: 'Marten Do',
+        description: "Re-create the website for Delivery company",
+        tags: ['HTML5', 'CSS3', 'JavaScript', 'PWA'],
+
+    }, {
+
+        id: 'p15',
+        title: 'Tutori Dashboard',
+        description: "A full complex Dashboard with alot of functions to manage an online Institues Platform.",
+        tags: ['ReactJs', 'Redux', 'React-Bootstrap'],
+
+    }, {
+
+        id: 'p16',
+        title: 'Arabesque',
+        description: "Full E-commerce and dashboard for selling soaps and coppers",
+        tags: ['ReactJs', 'Redux', 'React-Bootstrap', 'PWA'],
+
+    }, {
+
+        id: 'p17',
+        title: 'Xo',
+        description: "Full E-commerce and dashboard for selling clothes and other products",
+        tags: ['ReactJs', 'Redux', 'React-Bootstrap'],
+
     }
 ]
